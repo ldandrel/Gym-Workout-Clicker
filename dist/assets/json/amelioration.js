67c10fac082423ea10c35d182d653a155704104b
Luc Dandrel <luc.dandrel@gmail.com>
2016-12-13 14:22:03 +0100

    update json

--- dist/assets/json/amelioration.js
+++ dist/assets/json/amelioration.js
@@ -8,58 +8,71 @@ amelioration = JSON.parse(amelioration);
 
 if (!amelioration)
 {
-	amelioration =
+	amelioration.page_1 = 
 	[
 	{
-		'name'    : 'Poids',
-		'value'   : 50,
-		'fame'    : 0,
-		'level'   : 0,
-		'upgrade' : [
+		'name'        : 'Haltères',
+		'description' : '',
+		'strength'	  : 15,
+		'fame'        : 0,
+		'level'       : 0,
+		'results'     : 'click_value',
+		'icon'        : 'halteres.png',
+		'again'       : true,
+		'bought'      : false,
+		'upgrade'     : [
 		{
-			'name'   : 'alter',
-			'carac'  : 'force1',
-			'icon'   : 'image-2.png',
-			'again'  : false,
-			'bought' : false
+			'name'        : 'Matières',
+			'description' : 'Changez les matériaux de vos haltères : plastique, bois, acier, fonte... Plus vous en achetez plus vous serez fort',
+			'carac'       : '*2_halteres',
+			'strength'    : '100',
+			'icon'        : 'upgrade.png',
+			'again'       : true,
+			'bought'      : false
 		},
-		'name'    : 'Poids',
-		'value'   : 50,
-		'fame'    : 0,
-		'level'   : 0,
-		'upgrade' : [
 		{
-			'name'   : 'alter',
-			'carac'  : 'force1',
-			'icon'   : 'image-2.png',
-			'again'  : false,
-			'bought' : false
+			'name'        : 'Poids',
+			'description' : 'Changez le poids de vos haltères. Plus en seront lourdes plus vous serez fort !',
+			'carac'       : '*4_halteres',
+			'strength'    : '500',
+			'icon'        : 'upgrade.png',
+			'again'       : true,
+			'bought'      : false,
+
 		},
-		];
+		]
 	},
 	{
-		'name'  : 'Get on social Medias',
-		'value' : 100,
-		'fame'  : 0,
-		'level' : 0
-	},
-	{
-		'name'  : 'Ceinture d\'abdos',
-		'value' : 1000,
-		'fame'  : 50,
-		'level' : 0
-	},
-	{
-		'name'  : 'Epilations',
-		'value' : 1000,
-		'fame'  : 50,
-		'level' : 0
-	},
-	{
-		'name'  : 'Conventions',
-		'value' : 2000,
-		'fame'  : 500,
-		'level' : 0
-	},
+		'name'        : 'Équipements',
+		'description' : '',
+		'strength'	  : 100,
+		'fame'        : 0,
+		'level'       : 0,
+		'results'     : 'click_value',
+		'icon'        : 'halteres.png',
+		'again'       : true,
+		'bought'      : false,
+		'upgrade'     : [
+		{
+			'name'        : 'Ceinture abdominale',
+			'description' : 'Achetez une ceinture abdominale pour devenir plus fort !',
+			'carac'       : '*2_equipement',
+			'strength'    : '1000',
+			'icon'        : 'upgrade.png',
+			'again'       : false,
+			'bought'      : false
+		},
+		{
+			'name'        : 'Bracelet connecté',
+			'description' : 'Achetez un brancelet connecté pour être  !',
+			'carac'       : '+1%_strenght',
+			'strength'    : '1000000000',
+			'icon'        : 'upgrade.png',
+			'again'       : false,
+			'bought'      : false,
+
+		},
+		]
+	},	
 	];
 }
